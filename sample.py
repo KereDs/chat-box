44737e877f0cb3a80c3b69cb3f48f1d9a490f6fd
KereDs <52886982+KereDs@users.noreply.github.com>
2019-07-15 11:45:40 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -40,7 +40,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     # 回應使用者輸入的
-  if "貼圖" in message or "sticker" in message:
+  if "貼圖" in msg or "sticker" in msg:
         message=StickerSendMessage(
             package_id='1',sticker_id='1')
         line_bot_api.reply_message(
